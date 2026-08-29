876d905a136bb17b610b305e80931a683401fe78
Хижа Кирилл <scerep529@gmail.com>
2026-08-29 23:13:13 +0600

    fix

--- src/features/reports/ui/ReportsDetailList.tsx
+++ src/features/reports/ui/ReportsDetailList.tsx
@@ -44,7 +44,7 @@ export function ReportsDetailCard({ report }: IProps) {
                 <div className="flex gap-2.5 ">
                     <div className="w-2 h-2 rounded-full bg-red-400 mt-1.25" />
                     <div className="flex flex-col gap-2.5">
-                        <h6 className="text-sm font-bold">Препятсвия:</h6>
+                        <h6 className="text-sm font-bold">Препятствия:</h6>
                         <ul className="flex flex-col gap-1.5 pl-4">
                             {report.blockers.map((completed) => (
                                 <li
