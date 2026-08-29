cda364f38a9cc378e56df0b64273620e868179a2
Хижа Кирилл <scerep529@gmail.com>
2026-08-30 02:09:09 +0600

    feat: change telegram provider

--- src/widgets/layout/Header.tsx
+++ src/widgets/layout/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
                 sticky top-0 z-50
                 border-b border-border
                 bg-header
-                pt-[var(--app-content-safe-top)]
+                pt-[calc(var(--app-content-safe-top) + var(--app-safe-top))]
             "
         >
             <Container className="flex h-[56px] items-center justify-between">
